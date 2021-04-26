7fcf1b4e9211a332c6d95e65a9abf7ebe8e8a0a5
cazz7 <cazz182@gmail.com>
2021-04-26 02:39:23 +0000

    Desarrollo de 13 puntos

--- Lists/webapp/controller/ListTypes.controller.js
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -1,15 +1,86 @@
+// @ts-nocheck
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Lists.controller.ListTypes", {
-			onInit: function () {
+        return Controller.extend("logaligroup.Lists.controller.ListTypes", {
+            onInit: function () {
+                var oJSONModel = new sap.ui.model.json.JSONModel();
+                oJSONModel.loadData("./localService/mockdata/ListData.json");
+                this.getView().setModel(oJSONModel);
+            },
+            getGroupHeader: function (oGroup) {
+                var groupHeaderListItem = new sap.m.GroupHeaderListItem({
+                    title: oGroup.key,
+                    upperCase: true
+                });
 
-			}
-		});
-	});
+                return groupHeaderListItem;
+            },
+            onShowSelectedRow: function () {
+                var standardList = this.getView().byId("standardList");
+                var selectedItems = standardList.getSelectedItems();
+
+                var i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if (selectedItems === 0) {
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"));
+                } else {
+
+                    var textMessage = i18nModel.getText("selection");
+
+                    for (var item in selectedItems) {
+                        var context = selectedItems[item].getBindingContext();
+                        var oContext = context.getObject();
+                        textMessage = textMessage + " - " + oContext.Material;
+                    };
+
+                    sap.m.MessageToast.show(textMessage);
+                }
+            },
+            onDeleteSelectedRow: function () {
+                var standardList = this.getView().byId("standardList");
+                var selectedItems = standardList.getSelectedItems();
+
+                var i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if (selectedItems === 0) {
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"));
+                } else {
+
+                    var textMessage = i18nModel.getText("selection");
+                    var model = this.getView().getModel();
+                    var products = model.getProperty("/Products");
+
+                    var textMessage = i18nModel.getText("selection");
+
+                    //Deleted items
+                    var arrayId = [];
+
+                    for (var i in selectedItems) {
+                        var context = selectedItems[i].getBindingContext();
+                        var oContext = context.getObject();
+
+                        arrayId.push(oContext.Id);
+
+                        textMessage = textMessage + " - " + oContext.Material;
+                    }
+
+                    //Se obtienen productos no seleccionados
+                    products = products.filter(function (p) {
+                        return !arrayId.includes(p.Id);
+                    });
+
+                    model.setProperty("/Products", products);
+                    // To clear selected items
+                    standardList.removeSelections();
+                    sap.m.MessageToast.show(textMessage);
+                }
+            }
+        });
+    });
